078dc125745193394d5762443442b665e6cc0170
NithinManupuri <149311779+NithinManupuri@users.noreply.github.com>
2024-05-08 15:00:26 +0530

    Update AdminChatRepo.java

--- src/main/java/in/spring/repository/AdminChatRepo.java
+++ src/main/java/in/spring/repository/AdminChatRepo.java
@@ -2,6 +2,7 @@ package in.spring.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query; 
 
 import in.spring.entity.AdminChat;
 
